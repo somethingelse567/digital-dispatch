1460d112ea1a2246a08295292b914ca11e8ffb32
vitotomas <64912521+vitotomas@users.noreply.github.com>
2021-12-01 14:13:24 +0100

    Add files via upload

---
+++ bing_maps_test/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using bing_maps_test.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace bing_maps_test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            LocationLists model = new LocationLists();
+            var locations = new List<Locations>()
+            {
+                new Locations(1, "Bhubaneswar","Bhubaneswar, Odisha", 20.296059, 85.824539),
+                new Locations(2, "Hyderabad","Hyderabad, Telengana", 17.387140, 78.491684),
+                new Locations(3, "Bengaluru","Bengaluru, Karnataka", 12.972442, 77.580643)
+            };
+            model.LocationList = locations;
+            return View(model);
+        }
+    }
+}
